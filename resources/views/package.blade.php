32375ff77c01f81917f2c02914b99b846d1c39e0
Abdurashid-dev <abdurashiddev@gmail.com>
2022-12-12 10:06:10 +0500

    some changes

--- resources/views/package.blade.php
+++ resources/views/package.blade.php
@@ -8,17 +8,17 @@
             <h2>{{$package->name}}</h2>
             <hr>
             <div class="row">
-                <div class="col-md-8 col-sm-12">
+                <div class="col-md-8 col-sm-12 mb-3">
                     <p>{!! $package->description !!}</p>
                     <hr>
                     <p>IMAGES</p>
                     @if(!is_null($package->images))
                         @foreach($package->images as $image)
-                            <img src="{{asset($image->image)}}" alt="package" style="width: 400px">
+                            <img src="{{asset($image->image)}}" class="img-thumbnail img-responsive" alt="package" style="width: 400px">
                         @endforeach
                     @endif
                 </div>
-                <div class="col-md-4 col-sm12">
+                <div class="col-md-4 col-sm-12 mb-3">
                     <table class="table table-bordered">
                         <tr>
                             <th colspan="2" class="text-center">Information</th>
@@ -56,7 +56,7 @@
                         </tr>
                         <tr>
                             <th>Link:</th>
-                            <td><a href="{{$package->link}}">{{$package->link}}</a></td>
+                            <td><a href="{{$package->link}}">Link</a></td>
                         </tr>
                     </table>
 
